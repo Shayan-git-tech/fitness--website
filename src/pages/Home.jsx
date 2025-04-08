d02372947af69a4f61739ab144c6aa6004acf9e9
Shayan-git-tech <shayankhan12340@gmail.com>
2025-04-08 20:40:19 +0500

    Updated code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { Box } from '@mui/material'
 import HeroBanner from '../components/HeroBanner'
 import Exercises from '../components/Exercises'
 import SearchExercises from '../components/SearchExercises'
-import "react-horizontal-scrolling-menu/dist/styles.css";
 
 const Home = () => {
   const [bodyPart, setBodyPart] = useState('all')
